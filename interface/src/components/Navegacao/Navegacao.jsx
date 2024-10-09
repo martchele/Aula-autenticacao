f523c8adad4b090f63bfdcc6a4a6a336c42e8e3a
Marcília <marcilia.vitoria2006@gmail.com>
2024-10-09 16:46:37 -0300

    Aula do dia 09/10

--- interface/src/components/Navegacao/Navegacao.jsx
+++ interface/src/components/Navegacao/Navegacao.jsx
@@ -1,9 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/esm/Button';
 
 function Navegacao() {
+
+    // Gerencia o estado de autenticação
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    // Verifica o token de autenticação no localStorage
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setIsAuthenticated(true);
+        }
+    }, []);
+
+    // Lida com o logout
+    const handleLogout = () => {
+        localStorage.clear();  // Remove todos os itens do localStorage
+        setIsAuthenticated(false);  // Atualiza o estado de autenticação
+        window.location.href = '/';  // Redireciona para a página inicial
+    };
+
+    // Lida com o login
+    const handleLogin = () => {
+        window.location.href = '/login';  // Redireciona para a página de login
+    };
 
     const estiloNavbar = {
         backgroundColor: 'var(--primaryColor)',
@@ -21,8 +45,11 @@ function Navegacao() {
                     <Nav className="me-auto">
                         <Nav.Link href="/pessoas" style={estiloNavOptions}>Pessoas</Nav.Link>
                     </Nav>
-                    <button id="logoutButtonNovo">Sair da Conta</button>
-                    <Button href='/login' variant='light'>Login</Button>
+                    {isAuthenticated ? (
+                        <Button onClick={handleLogout} variant='light'>Logout</Button>
+                    ) : (
+                        <Button onClick={handleLogin} variant='light'>Login</Button>
+                    )}
                 </Container>
             </Navbar>
         </>
